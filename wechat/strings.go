cbd07d74289dabbdc7be7ec17a7d7970777d356b
Cluas <Cluas@live.cn>
2020-04-15 22:44:33 +0800

    docs: update document.

--- wechat/strings.go
+++ wechat/strings.go
@@ -1,8 +1,3 @@
-// Copyright 2013 The go-github AUTHORS. All rights reserved.
-//
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package wechat
 
 import (
